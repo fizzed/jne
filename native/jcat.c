7a6444a22b113f84230a299b76b77c34c9c3345f
Joe Lauer <joe@lauer.bz>
2023-02-15 14:06:08 -0500

    New buildfile for jcat

--- native/jcat.c
+++ native/jcat.c
@@ -6,30 +6,28 @@ void cat_it(FILE *fp)
 {
   char buf[BUFSIZ];
 
-  while(fgets(buf, sizeof(buf), fp))
+  while (fgets(buf, sizeof(buf), fp)) {
     fputs(buf, stdout);
+  }
 }
 
 int main(int argc, char **argv)
 {
-  if(argc < 2)
+  if (argc < 2) {
     cat_it(stdin);
-  else
-  {
+  } else {
     int i;
     FILE *fp;
-
     for(i = 1;argv[i];++i)
     {
-      if(!(fp = fopen(argv[i], "r")))
-        fprintf(stderr, "mycat: %s: %s\n", argv[i], strerror(errno));
-      else
-      {
+      if (!(fp = fopen(argv[i], "r"))) {
+        fprintf(stderr, "jcat: %s: %s\n", argv[i], strerror(errno));
+        return 1;
+      } else {
         cat_it(fp);
         fclose(fp);
       }
     }
   }
-
   return 0;
 }
