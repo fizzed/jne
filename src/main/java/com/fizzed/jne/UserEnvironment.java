bd85fbfc3685480975e898db25af43f7b74a9f88
Joe Lauer <joe@lauer.bz>
2025-09-29 17:07:43 -0400

    Support for environment on windows

--- src/main/java/com/fizzed/jne/UserEnvironment.java
+++ src/main/java/com/fizzed/jne/UserEnvironment.java
@@ -1,7 +1,28 @@
 package com.fizzed.jne;
 
+/*-
+ * #%L
+ * jne
+ * %%
+ * Copyright (C) 2016 - 2025 Fizzed, Inc
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.fizzed.jne.internal.EtcPasswd;
 import com.fizzed.jne.internal.MacDscl;
+import com.fizzed.jne.internal.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,8 +89,12 @@ public class UserEnvironment {
     static private UserEnvironment detect(boolean logical) {
         final UserEnvironment userEnvironment = new UserEnvironment();
 
-        // 1. detect which user we are running as
+        // 1. detect which user we are running as (this is most linux/unix/mac systems)
         String user = trimToNull(System.getenv("USER"));
+        // on windows, this usually is USERNAME
+        if (user == null) {
+            user = System.getenv("USERNAME");
+        }
 
         // 2. detect if running with elevated privileges (e.g. sudo/doas) since that will effect what "user" we
         // think we are running as
@@ -77,6 +102,9 @@ public class UserEnvironment {
         if (elevatedUser == null) {
             elevatedUser = trimToNull(System.getenv("DOAS_USER"));
         }
+
+        // NOTE: on windows, we could try to detect if a process has elevated privileges or not, but that's a lot of
+        // work just to set a boolean in our user environment, so we'll skip it for now
 
         // 3. are going to proceed with the effective or logical user?
         userEnvironment.user = user;
@@ -109,10 +137,10 @@ public class UserEnvironment {
             }
         }
 
-        // we now need to know what operating system we're on so we can detect things faster
+        // second, we now need to know what operating system we're on so we can detect things faster
         final OperatingSystem os = PlatformInfo.detectOperatingSystem();
 
-        // second, we may be on a macos, which requires using (dscl . -read /Users/builder) to determine a shell present
+        // we may be on a macos, which requires using (dscl . -read /Users/builder) to determine a shell present
         if (os == OperatingSystem.MACOS) {
             try {
                 MacDscl macDscl = MacDscl.readByUser(userEnvironment.user);
@@ -130,7 +158,28 @@ public class UserEnvironment {
             }
         }
 
-        log.info("");
+        if (os == OperatingSystem.WINDOWS) {
+            // environment variables (regardless of whether we are elevated or not) will setup everything nicely
+            final String homeDrive = trimToNull(System.getenv("HOMEDRIVE"));
+            final String homePath = trimToNull(System.getenv("HOMEPATH"));
+            if (homeDrive != null && homePath != null) {
+                userEnvironment.homeDir = Paths.get(homeDrive + homePath);
+            }
+
+            // on windows we are going to simplify things a bit as a user does not really pick their shell
+            // however, if powershell exists, we will make that the user's preferred shell, otherwise it'll be cmd.exe
+            final Path powershellExe = Utils.which("powershell.exe");
+            if (powershellExe != null) {
+                userEnvironment.shell = powershellExe;
+                userEnvironment.shellType = ShellType.PS;
+            } else {
+                final Path cmdExe = Utils.which("cmd.exe");
+                if (cmdExe != null) {
+                    userEnvironment.shell = cmdExe;
+                    userEnvironment.shellType = ShellType.CMD;
+                }
+            }
+        }
     }
 
 }
--- src/main/java/com/fizzed/jne/internal/Utils.java
+++ src/main/java/com/fizzed/jne/internal/Utils.java
@@ -1,8 +1,32 @@
 package com.fizzed.jne.internal;
 
+/*-
+ * #%L
+ * jne
+ * %%
+ * Copyright (C) 2016 - 2025 Fizzed, Inc
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Utils {
@@ -17,45 +41,44 @@ public class Utils {
         return value;
     }
 
+    static public Path which(String nameOfExeOrBin) {
+        // search PATH for existence of an executable
+        final String path = System.getenv("PATH");
+        if (path != null) {
+            final String[] paths = path.split(File.pathSeparator);
+            for (String p : paths) {
+                Path pathDir = Paths.get(p);
+                Path exeFile = pathDir.resolve(nameOfExeOrBin);
+                if (Files.exists(exeFile)) {
+                    return exeFile;
+                }
+            }
+        }
+        return null;
+    }
+
     static public String execAndGetOutput(List<String> commands) throws IOException, InterruptedException {
-            // Create a ProcessBuilder instance
-            final ProcessBuilder processBuilder = new ProcessBuilder(commands.toArray(new String[0]));
+        // Create a ProcessBuilder instance
+        final ProcessBuilder processBuilder = new ProcessBuilder(commands.toArray(new String[0]));
 
-            // Start the process
-            final Process process = processBuilder.start();
+        // Start the process
+        final Process process = processBuilder.start();
 
-            // Read the output from the process's input stream (standard output)
-            final StringBuilder output = new StringBuilder();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    output.append(line).append(System.lineSeparator());
-                }
+        // Read the output from the process's input stream (standard output)
+        final StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(System.lineSeparator());
             }
+        }
 
-            // Wait for the process to complete and get its exit code
-            int exitCode = process.waitFor();
+        // Wait for the process to complete and get its exit code
+        int exitCode = process.waitFor();
 
-            if (exitCode != 0) {
-                throw new IOException("Process exited with a non-zero code (actual " + exitCode + ")");
-            }
-
-            //System.out.println("Process exited with code: " + exitCode);
-            //System.out.println("Process Output:");
-            //System.out.println(output.toString());
-
-            // You can also read the error stream if needed
-            /*StringBuilder errorOutput = new StringBuilder();
-            try (BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
-                String errorLine;
-                while ((errorLine = errorReader.readLine()) != null) {
-                    errorOutput.append(errorLine).append(System.lineSeparator());
-                }
-            }
-            if (errorOutput.length() > 0) {
-                System.err.println("Process Error Output:");
-                System.err.println(errorOutput.toString());
-            }*/
+        if (exitCode != 0) {
+            throw new IOException("Process exited with a non-zero code (actual " + exitCode + ")");
+        }
 
         return output.toString();
     }
