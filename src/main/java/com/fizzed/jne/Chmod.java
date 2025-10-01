1a9f397fd8f7e4bd0b807d198cf5d2e5155c03ed
Joe Lauer <joe@lauer.bz>
2025-10-01 14:09:24 -0400

    Improve chmod that works on windows

--- src/main/java/com/fizzed/jne/Chmod.java
+++ src/main/java/com/fizzed/jne/Chmod.java
@@ -1,10 +1,33 @@
 package com.fizzed.jne;
 
+/*-
+ * #%L
+ * jne
+ * %%
+ * Copyright (C) 2016 - 2025 Fizzed, Inc
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Chmod {
@@ -25,8 +48,9 @@ public class Chmod {
         try {
             Files.setPosixFilePermissions(path, permission.getPosixFilePermissions());
         } catch (UnsupportedOperationException e) {
+            applyWindowsAclWithAdminOverride(path, permission.getPosixFilePermissions());
             // likely just not on posix, and we'll fallback to old "file" methods instead
-            applyLegacyPermissions(path.toFile(), permission.getPosixFilePermissions());
+            //applyLegacyPermissions(path.toFile(), permission.getPosixFilePermissions());
         }
     }
 
@@ -97,6 +121,157 @@ public class Chmod {
         // to 'r--' (read) *should* set the owner's read bit to true, but might
         // disable the owner's write/execute bits if the underlying OS implementation
         // is very crude.
+    }
+
+    /**
+     * Maps a Set<PosixFilePermission> to a Windows ACL list, with the following strategy:
+     * 1. Grants the File Owner the corresponding POSIX permissions.
+     * 2. Ignores the POSIX Group permissions.
+     * 3. Grants the 'Others' permissions to the 'Everyone' principal.
+     * 4. Grants the local 'Administrators' group Full Control.
+     *
+     * @param path The file or directory path.
+     * @param posixPerms The set of desired POSIX permissions (Owner and Others used).
+     * @throws IOException if an I/O error occurs or the ACL view is not supported.
+     */
+    static private void applyWindowsAclWithAdminOverride(Path path, Set<PosixFilePermission> posixPerms)
+        throws IOException, UnsupportedOperationException {
+
+        AclFileAttributeView aclView = Files.getFileAttributeView(path, AclFileAttributeView.class);
+        if (aclView == null) {
+            throw new UnsupportedOperationException("AclFileAttributeView is not supported on this file system.");
+        }
+
+        UserPrincipalLookupService lookupService = path.getFileSystem().getUserPrincipalLookupService();
+        List<AclEntry> aclEntries = new ArrayList<AclEntry>();
+
+        // --- 1. Lookup Principals ---
+        UserPrincipal owner = Files.getOwner(path);
+
+        // The Windows local Administrators group
+        GroupPrincipal administratorsGroup;
+        try {
+            administratorsGroup = lookupService.lookupPrincipalByGroupName("Administrators");
+        } catch (UserPrincipalNotFoundException e) {
+            throw new IOException("Could not find the 'Administrators' group principal.", e);
+        }
+
+        // 'Others' mapping
+        UserPrincipal everyonePrincipal;
+        try {
+            everyonePrincipal = lookupService.lookupPrincipalByName("EVERYONE@");
+        } catch (UserPrincipalNotFoundException e) {
+            everyonePrincipal = lookupService.lookupPrincipalByName("Everyone");
+        }
+
+
+        // --- 2. Define Permissions for each relevant identity (Owner, Others) ---
+        boolean isDirectory = Files.isDirectory(path);
+
+        // a) Owner Permissions
+        Set<AclEntryPermission> ownerAclPerms = mapPosixToAclPermissions(posixPerms, "OWNER", isDirectory);
+
+        // b) Others Permissions
+        Set<AclEntryPermission> othersAclPerms = mapPosixToAclPermissions(posixPerms, "OTHERS", isDirectory);
+
+        // c) Administrator FULL CONTROL
+        Set<AclEntryPermission> adminFullControl = new HashSet<AclEntryPermission>();
+
+        // Full Control includes all specific permissions
+        adminFullControl.add(AclEntryPermission.READ_ACL);
+        adminFullControl.add(AclEntryPermission.WRITE_ACL);
+        adminFullControl.add(AclEntryPermission.READ_ATTRIBUTES);
+        adminFullControl.add(AclEntryPermission.WRITE_ATTRIBUTES);
+        adminFullControl.add(AclEntryPermission.SYNCHRONIZE);
+        adminFullControl.add(AclEntryPermission.DELETE);
+        adminFullControl.add(AclEntryPermission.WRITE_OWNER);
+        adminFullControl.add(AclEntryPermission.READ_DATA);
+        adminFullControl.add(AclEntryPermission.EXECUTE);
+        adminFullControl.add(AclEntryPermission.WRITE_DATA);
+        adminFullControl.add(AclEntryPermission.APPEND_DATA);
+
+        // Directory-only permissions are still part of Full Control
+        adminFullControl.add(AclEntryPermission.DELETE_CHILD);
+        adminFullControl.add(AclEntryPermission.LIST_DIRECTORY);
+        adminFullControl.add(AclEntryPermission.ADD_FILE);
+        adminFullControl.add(AclEntryPermission.ADD_SUBDIRECTORY);
+
+        // --- 3. Build ACL Entries ---
+
+        // 1. Administrators Group (FULL CONTROL)
+        AclEntry adminEntry = AclEntry.newBuilder()
+            .setType(AclEntryType.ALLOW)
+            .setPrincipal(administratorsGroup)
+            .setPermissions(adminFullControl)
+            .setFlags(AclEntryFlag.FILE_INHERIT, AclEntryFlag.DIRECTORY_INHERIT)
+            .build();
+        aclEntries.add(adminEntry);
+
+        // 2. File Owner (POSIX Owner mapping)
+        if (!ownerAclPerms.isEmpty()) {
+            AclEntry ownerEntry = AclEntry.newBuilder()
+                .setType(AclEntryType.ALLOW)
+                .setPrincipal(owner)
+                .setPermissions(ownerAclPerms)
+                .setFlags(AclEntryFlag.FILE_INHERIT, AclEntryFlag.DIRECTORY_INHERIT)
+                .build();
+            aclEntries.add(ownerEntry);
+        }
+
+        // 3. Others (POSIX Others mapping)
+        if (!othersAclPerms.isEmpty()) {
+            AclEntry everyoneEntry = AclEntry.newBuilder()
+                .setType(AclEntryType.ALLOW)
+                .setPrincipal(everyonePrincipal)
+                .setPermissions(othersAclPerms)
+                .setFlags(AclEntryFlag.FILE_INHERIT, AclEntryFlag.DIRECTORY_INHERIT)
+                .build();
+            aclEntries.add(everyoneEntry);
+        }
+
+        // --- 4. Apply the new ACL list ---
+        aclView.setAcl(aclEntries);
+    }
+
+    // --- Helper Method ---
+
+    private static Set<AclEntryPermission> mapPosixToAclPermissions(
+        Set<PosixFilePermission> posixPerms, String principalType, boolean isDirectory) {
+
+        // Use HashSet as a generic, standard implementation of Set
+        Set<AclEntryPermission> aclPermissions = new HashSet<>();
+
+        // Lookup strings for PosixFilePermission
+        final String readPerm = principalType + "_READ";
+        final String writePerm = principalType + "_WRITE";
+        final String execPerm = principalType + "_EXECUTE";
+
+        boolean read = posixPerms.contains(PosixFilePermission.valueOf(readPerm));
+        boolean write = posixPerms.contains(PosixFilePermission.valueOf(writePerm));
+        boolean execute = posixPerms.contains(PosixFilePermission.valueOf(execPerm));
+
+        if (read) {
+            aclPermissions.add(AclEntryPermission.READ_DATA);
+            aclPermissions.add(AclEntryPermission.READ_ATTRIBUTES);
+            aclPermissions.add(AclEntryPermission.SYNCHRONIZE);
+            if (isDirectory) aclPermissions.add(AclEntryPermission.LIST_DIRECTORY);
+        }
+
+        if (write) {
+            aclPermissions.add(AclEntryPermission.WRITE_DATA);
+            aclPermissions.add(AclEntryPermission.APPEND_DATA);
+            aclPermissions.add(AclEntryPermission.WRITE_ATTRIBUTES);
+            if (isDirectory) {
+                aclPermissions.add(AclEntryPermission.ADD_FILE);
+                aclPermissions.add(AclEntryPermission.ADD_SUBDIRECTORY);
+            }
+        }
+
+        if (execute) {
+            aclPermissions.add(AclEntryPermission.EXECUTE);
+        }
+
+        return aclPermissions;
     }
 
 }
--- src/main/java/com/fizzed/jne/SystemFilePermission.java
+++ src/main/java/com/fizzed/jne/SystemFilePermission.java
@@ -1,5 +1,25 @@
 package com.fizzed.jne;
 
+/*-
+ * #%L
+ * jne
+ * %%
+ * Copyright (C) 2016 - 2025 Fizzed, Inc
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
@@ -13,7 +33,7 @@ public class SystemFilePermission {
     private SystemFilePermission(String octalPermission, String symbolicPermission) {
         this.octalPermission = octalPermission;
         this.symbolicPermission = symbolicPermission;
-        this.posixFilePermissions = PosixFilePermissions.fromString(octalPermission);
+        this.posixFilePermissions = PosixFilePermissions.fromString(symbolicPermission);
     }
 
     public String getOctalPermission() {
