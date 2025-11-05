24874e00919f9c5a564a449197612818565d6898
Joe Lauer <joe@lauer.bz>
2025-11-05 17:19:55 -0500

    Use incubating VisualStudios support for blaze.jar to streamline compiling native tests on windows

--- .blaze/blaze.java
+++ .blaze/blaze.java
@@ -1,28 +1,23 @@
 import com.fizzed.blaze.Contexts;
-import com.fizzed.blaze.Systems;
 import com.fizzed.blaze.Task;
 import com.fizzed.blaze.TaskGroup;
+import com.fizzed.blaze.incubating.VcVars;
 import com.fizzed.blaze.project.PublicBlaze;
-import com.fizzed.blaze.system.Exec;
 import com.fizzed.buildx.Buildx;
 import com.fizzed.buildx.Target;
 import com.fizzed.jne.NativeTarget;
 import com.fizzed.jne.OperatingSystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.fizzed.blaze.Contexts.withBaseDir;
 import static com.fizzed.blaze.Systems.*;
 import static com.fizzed.blaze.Systems.exec;
+import static com.fizzed.blaze.incubating.VisualStudios.vcVars;
+import static com.fizzed.blaze.incubating.VisualStudios.vcVarsExec;
 import static com.fizzed.blaze.util.Globber.globber;
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
 
 @TaskGroup(value="project", name="Project", order=1)
 @TaskGroup(value="maintainers", name="Maintainers Only", order=2)
@@ -37,161 +32,6 @@ public class blaze extends PublicBlaze {
         // mvn process-test-classes exec:exec -Dexec.classpathScope=test -Dexec.executable=java -Dexec.args="-cp %classpath com.fizzed.jne.JavaHomesDemo"
         exec("mvn", "process-test-classes", "exec:exec",
             "-Dexec.classpathScope=test", "-Dexec.executable=java", "-Dexec.args=-cp %classpath com.fizzed.jne.JavaHomesDemo").run();
-    }
-
-
-    static public class VcVarSession implements AutoCloseable {
-
-        private final Map<String,String> vcVars;
-        private final List<Path> vcPaths;
-
-        public VcVarSession(Map<String, String> vcVars, List<Path> vcPaths) {
-            this.vcVars = vcVars;
-            this.vcPaths = vcPaths;
-        }
-
-        public Map<String, String> getVcVars() {
-            return vcVars;
-        }
-
-        public List<Path> getVcPaths() {
-            return vcPaths;
-        }
-
-        @Override
-        public void close() throws Exception {
-            // do nothing
-        }
-
-    }
-
-    static public class VisualStudios {
-        static private final Logger log = LoggerFactory.getLogger(VisualStudios.class);
-
-        static public Exec vcVarsExec(VcVarSession vcVarSession, String command, Object... arguments) {
-            Exec exec = Systems.exec(command, arguments)
-                .paths(vcVarSession.getVcPaths());
-
-            vcVarSession.getVcVars().forEach(exec::env);
-
-            return exec;
-        }
-
-        static public VcVarSession vcVarsSession(String targetArch, int... preferredYears) {
-            final String vcVarsArch = resolveTargetArch(targetArch);
-
-            // if no preferred years, use the default ones
-            if (preferredYears == null || preferredYears.length == 0) {
-                preferredYears = new int[] { 2022, 2019, 2017 };
-            }
-
-            // search for vcvarsall.bat file
-            Path vcVarsAllBatFile = null;
-            for (int year :  preferredYears) {
-                Path f = Paths.get("C:\\Program Files\\Microsoft Visual Studio\\"+year+"\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat");
-                log.debug("Searching vcvars @ {}", f);
-                if (Files.exists(f)) {
-                    vcVarsAllBatFile = f;
-                    break;
-                }
-            }
-
-            if (vcVarsAllBatFile == null) {
-                throw new IllegalStateException("Could not find vcvarsall.bat for years " + Arrays.toString(preferredYears));
-            }
-
-            // get a snapshot of variables before running vcvarsall.bat
-            final String preEnvOutput = exec("cmd", "/c", "set")
-                .runCaptureOutput(false)
-                .toString();
-
-            // parse the before list into a map
-            final Map<String,String> preEnv = parseEnvVars(preEnvOutput);
-
-            // now call vcvarsall.bat, grab the adjusted env vars
-            log.info("Loading visual studio variables for {} from {}", vcVarsArch, vcVarsAllBatFile);
-
-            final String postEnvOutput = exec("cmd", "/c", "\"call \"" + vcVarsAllBatFile + "\" " + vcVarsArch + " & set\"")
-                .runCaptureOutput(false)
-                .toString();
-
-            // parse the after list into a map
-            final Map<String,String> postEnv = parseEnvVars(postEnvOutput);
-
-            // calculate changes
-            final Map<String,String> vcVars = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-            for (Map.Entry<String,String> afterEntry : postEnv.entrySet()) {
-                String envBeforeValue = preEnv.get(afterEntry.getKey());
-                if (envBeforeValue == null) {
-                    //log.info("NEW env: {} => {}",  afterEntry.getKey(), afterEntry.getValue());
-                    vcVars.put(afterEntry.getKey(), afterEntry.getValue());
-                } else if (!envBeforeValue.equals(afterEntry.getValue())) {
-                    //log.info("CHANGED env: {} => {}",  afterEntry.getKey(), afterEntry.getValue());
-                    vcVars.put(afterEntry.getKey(), envBeforeValue + "," + afterEntry.getValue());
-                } else {
-                    //log.info("SAME env: {} => {}",  afterEntry.getKey(), afterEntry.getValue());
-                }
-            }
-
-            // for some reason the PATH is case sensitive when calling execs, and the PATH contains an odd "," in
-            // some cases instead of ";", so we will remove the PATH, clean it up, and add it back
-            final String uncleanPath = vcVars.remove("PATH");
-
-            final List<Path> vcPaths = Arrays.stream(uncleanPath.split("[;,]"))
-                .map(Paths::get)
-                .collect(Collectors.toList());
-
-            // build a better, improved path and set it as all caps
-            final String sanitizedPath = vcPaths.stream()
-                .map(Object::toString)
-                .collect(joining(";"));
-
-            vcVars.put("PATH", sanitizedPath);
-
-            return new VcVarSession(vcVars, vcPaths);
-        }
-
-        static private Map<String,String> parseEnvVars(String output) {
-            final Map<String,String> envVars = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-
-            for (String line : output.split("\n")) {
-                String[] nv = line.trim().split("=");
-                if (nv.length == 2) {
-                    String name = nv[0].trim();
-                    String value = nv[1].trim();
-                    //log.info("{} => {}", name, value);
-                    envVars.put(name, value);
-                }
-            }
-
-            return envVars;
-        }
-
-        static private String resolveTargetArch(String targetArch) {
-            // arch can be a few things, that we need to match
-            final String hostArch = System.getProperty("os.arch");              // x86_64 or aarch64
-
-            if ("x64".equalsIgnoreCase(targetArch)) {
-                if ("x86_64".equalsIgnoreCase(hostArch) || "amd64".equalsIgnoreCase(hostArch)) {
-                    return "x64";
-                } else if ("aarch64".equalsIgnoreCase(hostArch)) {
-                    return "x64_arm64";
-                } else {
-                    throw new IllegalArgumentException("Unknown mapping from host arch " + hostArch + " to target arch " + targetArch);
-                }
-            } else if ("arm64".equalsIgnoreCase(targetArch)) {
-                if ("x86_64".equalsIgnoreCase(hostArch) || "amd64".equalsIgnoreCase(hostArch)) {
-                    return "arm64_x64";
-                } else if ("aarch64".equalsIgnoreCase(hostArch)) {
-                    return "arm64";
-                } else {
-                    throw new IllegalArgumentException("Unknown mapping from host arch " + hostArch + " to target arch " + targetArch);
-                }
-            } else {
-                throw new IllegalArgumentException("Unknown target arch " + targetArch + " (valid are x64 or arm64)");
-            }
-        }
-
     }
 
     @Task(group="project", order = 1, value="Builds native libraries and executables for the local os/arch")
@@ -212,14 +52,15 @@ public class blaze extends PublicBlaze {
         final String libname = nativeTarget.resolveLibraryFileName("helloj");
 
         if (nativeTarget.getOperatingSystem() == OperatingSystem.WINDOWS) {
-            try (VcVarSession vcVarsSession = VisualStudios.vcVarsSession(nativeTarget.getHardwareArchitecture().name())) {
+            // we may be cross-compiling so we pass the arch we want
+            try (VcVars vcVars = vcVars().arch(nativeTarget.getHardwareArchitecture().name()).verbose().run()) {
                 log.info("Building jcat executable...");
-                VisualStudios.vcVarsExec(vcVarsSession, "nmake", "-f", "VCMakefile")
+                vcVarsExec(vcVars, "nmake", "-f", "VCMakefile")
                     .workingDir(targetJcatDir)
                     .run();
 
                 log.info("Building helloj library...");
-                VisualStudios.vcVarsExec(vcVarsSession, "nmake", "-f", "VCMakefile")
+                vcVarsExec(vcVars, "nmake", "-f", "VCMakefile")
                     .workingDir(targetLibHelloJDir)
                     .run();
             }
