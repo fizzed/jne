58031cf5664bac04409ae01d7242524d0d627a9a
Joe Lauer <joe@lauer.bz>
2025-11-11 04:29:33 -0800

    Sample of how to get version for MacOS

--- src/test/java/com/fizzed/jne/internal/OsVersionDemo.java
+++ src/test/java/com/fizzed/jne/internal/OsVersionDemo.java
@@ -32,6 +32,17 @@ public class OsVersionDemo {
             final OsReleaseFile osReleaseFile = OsReleaseFile.parse(osReleaseContent);
 
             log.info("os-release: {}", osReleaseFile.getPrettyName());
+        } else if (nativeTarget.getOperatingSystem() == OperatingSystem.MACOS) {
+            // uname -a for kernel
+            // e.g. Linux bmh-jjlauer-4 6.17.0-6-generic #6-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct  7 13:34:17 UTC 2025 x86_64 GNU/Linux
+            // e.g. FreeBSD bmh-dev-x64-freebsd15-1 15.0-ALPHA4 FreeBSD 15.0-ALPHA4 stable/15-n280334-d2b670b27f37 GENERIC amd64
+            final String uname = runProcess("uname", "-a");
+            log.info("uname: {}", uname);
+
+            // sw_vers for distro (or it could be a .plist file we simply read)
+            final String swVersContent = runProcess("sw_vers");
+
+            log.info("sw_vers: {}", swVersContent);
         }
 
 
